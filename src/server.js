f0c23d8e1832483ec8b906730b296cfd9c750402
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-10-01 14:26:30 +0100

    server js ok

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import listEndpoints from "express-list-endpoints";
-import accommodationRouter from "./services/accommodation/index.js";
-const port = process.env.PORT;
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
+import accommodationRouter from './services/accommodation/index.js'
+import usersRouter from '../src/services/user/index.js'
 
-const mongoConnection = process.env.MONGO_CONNECTION_STRING;
+const port = process.env.PORT
 
-const server = express();
+const mongoConnection = process.env.MONGO_CONNECTION_STRING
 
-server.use(cors());
-server.use(express.json());
+const server = express()
+
+server.use(cors())
+server.use(express.json())
 
 server.use('/accommodation', accommodationRouter)
-
-
+server.use('/users', usersRouter)
 
 console.table(listEndpoints(server))
 server.listen(port, async () => {
@@ -22,9 +23,9 @@ server.listen(port, async () => {
     mongoose.connect(mongoConnection, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
-    console.log(`Server is running on port ${port} and is connected to db`);
+    })
+    console.log(`Server is running on port ${port} and is connected to db`)
   } catch (err) {
-    console.log("Db connection is faild", err);
+    console.log('Db connection is faild', err)
   }
-});
+})
